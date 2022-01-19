d2ddeeccb33cf5914b2d7c2199b7c88cea1f4213
Peter Kalanin <kalanin.peto@gmail.com>
2022-01-19 10:27:36 +0100

    add: UserList component

---
+++ src/app/user-list/user-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.scss']
+})
+export class UserListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
